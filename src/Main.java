b6914e8bfc79049be5a24e435a2b8f822469beed
passptiza17 <lisatest17@mail.ru>
2024-02-29 13:59:51 +0500

    add array in class main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random randomCycle = new Random();
+
+        int[] bicycles;
+        bicycles = new int[20];
+
+        for (int i = 0; i < bicycles.length; i++) {
+            bicycles[i] = randomCycle.nextInt(500);
+
+            System.out.println(bicycles[i]);
+        }
     }
 }
